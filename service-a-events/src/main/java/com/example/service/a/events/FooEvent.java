f4ad3fde617dafeb7ffec9950c6a06e49cd9997b
xdoo <claus.straube@gmail.com>
2017-02-06 14:59:29 +0100

    Domain Events

---
+++ service-a-events/src/main/java/com/example/service/a/events/FooEvent.java
@@ -0,0 +1,28 @@
+package com.example.service.a.events;
+
+/**
+ *
+ * @author claus
+ */
+public class FooEvent {
+    
+    private String valueA;
+    private String valueB;
+
+    public void setValueA(String valueA) {
+        this.valueA = valueA;
+    }
+
+    public String getValueA() {
+        return valueA;
+    }
+
+    public void setValueB(String valueB) {
+        this.valueB = valueB;
+    }
+
+    public String getValueB() {
+        return valueB;
+    }
+    
+}
