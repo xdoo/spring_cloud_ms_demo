bea6d6aecafe9a6035e30d741d7f35958aa1a6a4
xdoo <claus.straube@gmail.com>
2017-02-08 13:52:55 +0100

    implemented bean to avro mapping

--- service-a/src/main/java/com/example/FooProcessor.java
+++ service-a/src/main/java/com/example/FooProcessor.java
@@ -7,10 +7,14 @@ import org.springframework.messaging.MessageChannel;
  *
  * @author claus
  */
-public interface FooOutbound {
+public interface FooProcessor {
     
     public final String FOO_EVENT = "fooEvent";
+    public final String BAR_EVENT = "barEvent";
     
     @Output(FOO_EVENT)
     MessageChannel fooEvent();
+    
+    @Output(BAR_EVENT)
+    MessageChannel barEvent();
 }
--- service-c/src/main/java/com/example/FooEventHandler.java
+++ service-c/src/main/java/com/example/FooEventHandler.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.example.service.a.events.FooEvent;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
@@ -10,10 +9,10 @@ import org.springframework.stereotype.Service;
  * @author claus
  */
 @Service
-@EnableBinding(FooInbound.class)
+@EnableBinding(FooProcessor.class)
 public class FooEventHandler {
     
-    @StreamListener(FooInbound.INPUT_FOO_EVENT)
+    @StreamListener(FooProcessor.INPUT_FOO_EVENT)
     public void handleFooEvent(FooEvent fooEvent) {
         System.out.println(fooEvent.getValueA() + " und " + fooEvent.getValueB());
     }
