d8acfe3641cf50cb752c9fe2b64b242a64b5007e
xdoo <claus.straube@gmail.com>
2017-02-08 10:31:32 +0100

    added schema registry server

---
+++ schema-registry/src/main/java/com/example/Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.schema.server.EnableSchemaRegistryServer;
+
+@SpringBootApplication
+@EnableSchemaRegistryServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
