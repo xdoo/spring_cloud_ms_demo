4e88f3aa3459630823dad38118aac88ec8f1aae6
xdoo <claus.straube@gmail.com>
2017-02-08 10:36:04 +0100

    added schema registry clients

--- service-c/src/main/java/com/example/Application.java
+++ service-c/src/main/java/com/example/Application.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.schema.client.EnableSchemaRegistryClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSchemaRegistryClient
 public class Application {
 
 	public static void main(String[] args) {
